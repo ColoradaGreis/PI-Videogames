a7cf8cbd2747997f14715f3b5f00d5ce9d647f96
ColoradaGreis <graciana.baratti@gmail.com>
2023-07-30 21:46:14 -0300

    dotenv

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -25,7 +25,7 @@ const getApiInfo= async ()=>  {
         }
         return videogames 
     } catch (error) {
-        console.log(error)
+        console.log(error, "this is an error catched")
     }
 
 }
